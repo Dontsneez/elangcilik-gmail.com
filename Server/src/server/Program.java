da6bf906edbb9dc16a2b3e253d73e15b07ef34e0
Dontsneez <31280935+Dontsneez@users.noreply.github.com>
2020-03-27 22:05:31 +0700

    Add files via upload

---
+++ Server/src/server/Program.java
@@ -0,0 +1,22 @@
+package server;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import server.ui.Homechat;
+
+public final class Program {
+
+    private static void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        setSystemLookAndFeel();
+        new Homechat().setVisible(true);
+    }
+}
